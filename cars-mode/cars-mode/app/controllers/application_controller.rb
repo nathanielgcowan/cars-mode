ae46c15055f1811eb1beb762487e49cd377df302
Nathaniel Cowan <42325012+nathanielgcowan@users.noreply.github.com>
2020-10-03 19:25:17 -0500

    this is an update to the crud files
    I am updating the crud files to meet our criteria

--- cars-mode/cars-mode/app/controllers/application_controller.rb
+++ cars-mode/cars-mode/app/controllers/application_controller.rb
@@ -7,8 +7,33 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get "/" do
-    erb :welcome
+  get '/' do
+    redirect to "/cars/index.html"
+  end
+
+  helpers do
+
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(:email => session[:email]) if session[:email]
+    end
+    
+    
+    def login(email, password)
+      user = User.find_by(:email => email)
+      if user && user.authenticate(password)
+          session[:email] = user.email
+      else
+        redirect 'users/login'
+      end
+    end
+
+    def logout!
+      session.clear
+    end
   end
 
 end
--- cars-mode/cars-mode/app/controllers/cars_controller.rb
+++ cars-mode/cars-mode/app/controllers/cars_controller.rb
@@ -15,31 +15,31 @@ class CarsController < ApplicationController
   #Create POST: /cars
   post "/cars" do
     @car = Car.create(params)
-    redirect "/cars/#{@car.id}"
+    # redirect "/cars/#{@car.id}"
   end
 
   #Show GET: /cars/5
   get "/cars/:id" do
-    @car = Article.find(params[:id])
+    # @car = Article.find(params[:id])
     erb :"/cars/show.html"
   end
 
   #Edit GET: /cars/5/edit
   get "/cars/:id/edit" do
-    @car = Car.find(params[:id])
+    # @car = Car.find(params[:id])
     erb :"/cars/edit.html"
   end
 
   #Update PATCH: /cars/5
   patch "/cars/:id" do
-    @car = Car.find(params[:id])
+    # @car = Car.find(params[:id])
     @car.update(params[:car])
-    redirect "/cars/#{@car.id}"
+    # redirect "/cars/#{@car.id}"
   end
 
   #Destroy DELETE: /cars/5/delete
   delete "/cars/:id/delete" do
-    Car.destroy(params[:id])
+    # Car.destroy(params[:id])
     redirect "/cars"
   end
 end
